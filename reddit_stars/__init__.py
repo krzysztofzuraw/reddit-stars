e136b6c27603429f892f875803f29e915ec1a3bb
Krzysztof Żuraw <krzysztof.zuraw@gmail.com>
2016-05-21 20:16:55 +0200

    Intial commit; added reddit adapter

---
+++ reddit_stars/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Krzysztof Żuraw'
+__email__ = 'krzysztof.zuraw@gmail.com'
+__version__ = '0.0.1'
---
+++ reddit_stars/reddit_adapter.py
@@ -0,0 +1,49 @@
+import requests
+import requests.auth
+
+
+class RedditAdapter(object):
+    def __init__(
+        self, reddit_client_id, reddit_client_secret,
+        reddit_username, reddit_password
+    ):
+        self.reddit_client_id = reddit_client_id
+        self.reddit_client_secret = reddit_client_secret
+        self.reddit_username = reddit_username
+        self.reddit_password = reddit_password
+        self.reddit_token = None
+
+    def authorize(self):
+        client_auth = requests.auth.HTTPBasicAuth(
+            self.reddit_client_id,
+            self.reddit_client_secret
+        )
+        post_data = {
+            "grant_type": "password",
+            "username": self.reddit_username,
+            "password": self.reddit_password
+        }
+        headers = {"User-Agent": "RedditAdapter/0.1 by Krzysztof Zuraw"}
+        response = requests.post(
+            "https://www.reddit.com/api/v1/access_token",
+            auth=client_auth,
+            data=post_data,
+            headers=headers
+        )
+        self.reddit_token = response.json()['access_token']
+
+    def search(self, query, subreddit=None):
+        self.authorize()
+        headers = {
+            "Authorization": "bearer {token}".format(token=self.reddit_token),
+            "User-Agent": "RedditAdapter/0.1 by Krzysztof Zuraw"
+        }
+        response = requests.get(
+            "https://oauth.reddit.com/r/{subreddit}/"
+            "search.json?q={query}&restrict_sr={restrict}".format(
+                subreddit=subreddit,
+                query=query,
+                restrict='on' if subreddit else 'off'
+            ),
+            headers=headers)
+        return response.json()
